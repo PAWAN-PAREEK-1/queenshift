6704407dc95dfc4a202bab4da43132c6c0aa969e
pawan-pareek-1 <pawapareek506@gmail.com>
2025-12-15 16:34:58 +0530

    update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import { connectDB } from "./models/db.js";
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit:"50mb"}));
 dotenv.config();
 // connect to MongoDB
 
