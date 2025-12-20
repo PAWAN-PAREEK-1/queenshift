bc9a73cf13e841ae96c15cd14aef9d7f759624eb
pawan-pareek-1 <pawapareek506@gmail.com>
2025-12-20 14:03:52 +0530

    update

---
+++ models/LeagueProgress.js
@@ -0,0 +1,34 @@
+// models/leagueProgress.js
+import mongoose from "mongoose";
+
+const LeagueProgressSchema = new mongoose.Schema(
+  {
+    playerId: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+    },
+
+    total_score: {
+      type: Number,
+      default: 0,
+    },
+
+    league: {
+      name: {
+        type: String, // gold | diamond
+        default: "gold",
+      },
+      level: {
+        type: Number, // 1 | 2 | 3
+        default: 1,
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+
+
+export default mongoose.model("LeagueProgress", LeagueProgressSchema);
