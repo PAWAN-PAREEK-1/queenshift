b70c1ce99bbe14e8095ce372ff5b4e1af5bce294
PAWAN-PAREEK-1 <132331797+PAWAN-PAREEK-1@users.noreply.github.com>
2025-12-23 14:33:27 +0530

    Update

--- leagueRules.js
+++ leagueRules.js
@@ -1,12 +1,1 @@
-// leagueRules.js
-export const LEAGUES = [
-  // GOLD (lower league)
-  { name: "gold", level: 1, min: 0, max: 49 },
-  { name: "gold", level: 2, min: 50, max: 89 },
-  { name: "gold", level: 3, min: 90, max: 179 },
-
-  // DIAMOND (higher league)
-  { name: "diamond", level: 1, min: 180, max: 249 },
-  { name: "diamond", level: 2, min: 250, max: 349 },
-  { name: "diamond", level: 3, min: 350, max: Infinity },
-];
+export const LEAGUES = [  { name: "bronze", level: 3, min: 0, max: 49 }, { name: "bronze", level: 2, min: 50, max: 89 }, { name: "bronze", level: 1, min: 90, max: 179 },  { name: "gold", level: 3, min: 180, max: 249 }, { name: "gold", level: 2, min: 250, max: 349 }, { name: "gold", level: 1, min: 350, max: Infinity }, ];
