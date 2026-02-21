1fadc3d010b5e5623c726037798fc497862c2b8f
pawan-pareek-1 <pawapareek506@gmail.com>
2026-02-21 20:47:56 +0530

    db update

--- models/db.js
+++ models/db.js
@@ -2,22 +2,22 @@ import mongoose from "mongoose";
 
 let cached = global.mongoose;
 
-if (!cached) cached = global.mongoose = { conn: null, promise: null };
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
 
 export async function connectDB() {
   if (cached.conn) {
-    return cached.conn; // reuse existing connection
+    return cached.conn;
   }
 
   if (!cached.promise) {
     cached.promise = mongoose.connect(process.env.MONOGO_URL, {
       bufferCommands: false,
-    }).then((db) => {
-      console.log("✅ MongoDB connected");
-      return db;
     });
   }
 
   cached.conn = await cached.promise;
+  console.log("✅ MongoDB connected");
   return cached.conn;
 }
