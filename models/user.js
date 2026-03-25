3b3b27b989347c588487e1a592fc02875ecf88f7
pawan-pareek-1 <pawapareek506@gmail.com>
2026-03-25 22:31:24 +0530

    timestamp added

--- models/user.js
+++ models/user.js
@@ -5,44 +5,48 @@ const levelProgressSchema = new mongoose.Schema({
   // Stores total time taken for each level
   level_times: {
     type: Map,
-    of: Number,  // { "1": 23, "2": 30 ... seconds }
-    default: {}
+    of: Number, // { "1": 23, "2": 30 ... seconds }
+    default: {},
   },
-  average_time: { type: Number, default: 0 }
+  average_time: { type: Number, default: 0 },
 });
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    unique: true,
-    required: true,
-    trim: true
-  },
-  avatar_index: { type: Number, default: 0 },
-  frame_index: { type: Number, default: 0 },
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    avatar_index: { type: Number, default: 0 },
+    frame_index: { type: Number, default: 0 },
 
-  levels: {
-    easy: { type: levelProgressSchema, default: () => ({}) },
-    medium: { type: levelProgressSchema, default: () => ({}) },
-    hard: { type: levelProgressSchema, default: () => ({}) },
-    expert: { type: levelProgressSchema, default: () => ({}) },
-    dailyquest: { type: levelProgressSchema, default: () => ({}) },
-    weeklychallenge: { type: levelProgressSchema, default: () => ({}) },
-    thetower: { type: levelProgressSchema, default: () => ({}) },
-    timerush: { type: levelProgressSchema, default: () => ({}) },
-    twistermode: { type: levelProgressSchema, default: () => ({}) },
-  },
-    playerId:{
-    type:String,
-    required:true
-  },
- 
-    email:{
-      type:String,
-      unique:true,
-      sparse: true
-    }
+    levels: {
+      easy: { type: levelProgressSchema, default: () => ({}) },
+      medium: { type: levelProgressSchema, default: () => ({}) },
+      hard: { type: levelProgressSchema, default: () => ({}) },
+      expert: { type: levelProgressSchema, default: () => ({}) },
+      dailyquest: { type: levelProgressSchema, default: () => ({}) },
+      weeklychallenge: { type: levelProgressSchema, default: () => ({}) },
+      thetower: { type: levelProgressSchema, default: () => ({}) },
+      timerush: { type: levelProgressSchema, default: () => ({}) },
+      twistermode: { type: levelProgressSchema, default: () => ({}) },
+    },
+    playerId: {
+      type: String,
+      required: true,
+    },
 
-});
+    email: {
+      type: String,
+      unique: true,
+      sparse: true,
+    },
+  },
+  {
+    timestamps: true, // ✅ THIS ADDS createdAt & updatedAt
+  },
+);
 
 export default mongoose.model("User", userSchema);
